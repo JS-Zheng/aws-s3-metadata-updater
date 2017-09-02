676a0e179275e59a1683c13697a5cfb97754c5d5
Jasonnn <jasonnn331@gmail.com>
2017-09-02 12:14:01 +0800

    Prevent S3 1000 limits.

--- S3MetadataUpdater.java
+++ S3MetadataUpdater.java
@@ -6,18 +6,26 @@ import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by zhengzhongsheng on 2017/5/8.
+ * https://blog.jason.party
+ * <br>
+ * Please import AWS Java SDK first!
+ * http://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-install.html
  */
 public class S3MetadataUpdater {
 
-    public static final int FORMAT_UNSUPPORTED = 0;
-    public static final int FORMAT_JPEG = 1;
-    public static final int FORMAT_PNG = 2;
+    private static final int FORMAT_UNSUPPORTED = 0;
+    private static final int FORMAT_JPEG = 1;
+    private static final int FORMAT_PNG = 2;
 
-    public static final String bucketName = "your-bucket-name";
+    // Please update these fields
+    private static final String YOUR_BUCKET_NAME = "your-bucket-name";
+    private static final String YOUR_BUCKET_PATH = "wp-content/uploads/";
+    private static final long MAX_AGE = 31536000L;
 
     public static void main(String args[]) {
 
@@ -37,11 +45,14 @@ public class S3MetadataUpdater {
             System.out.println("Getting Started with Amazon S3");
             System.out.println("===========================================\n");
 
+            ObjectListing ol = s3.listObjects(YOUR_BUCKET_NAME, YOUR_BUCKET_PATH);
+            List<S3ObjectSummary> objects = getObjectsExclude1000Limits(s3, ol);
 
-            ObjectListing ol = s3.listObjects(bucketName);
-            List<S3ObjectSummary> objects = ol.getObjectSummaries();
 
             for (S3ObjectSummary os : objects) {
+                /*
+                 * do something.
+                 */
                 updateContentType(s3, os);
             }
 
@@ -51,9 +62,21 @@ public class S3MetadataUpdater {
 
     }
 
-    static void updateContentType(AmazonS3 s3, S3ObjectSummary os) {
+    // Prevent S3 1000 limits.
+    static List<S3ObjectSummary> getObjectsExclude1000Limits(AmazonS3 s3, ObjectListing ol) {
+        List<S3ObjectSummary> result = ol.getObjectSummaries();
+        while (ol.isTruncated()) {
+            ol = s3.listNextBatchOfObjects(ol);
+            result.addAll(ol.getObjectSummaries());
+        }
+        return result;
+    }
+
+
+    // It's just example. Please use your custom rules.
+    public static void updateContentType(AmazonS3 s3, S3ObjectSummary os) {
         String fileKey = os.getKey();
-        System.out.println("* " + os.getKey());
+        System.out.println("* " + fileKey);
 
         int format = FORMAT_UNSUPPORTED;
 
@@ -69,15 +92,22 @@ public class S3MetadataUpdater {
 
             ObjectMetadata newMeta = new ObjectMetadata();
             newMeta.setContentType(ContentTypeValue);
+            newMeta.setCacheControl("max-age=" + String.valueOf(MAX_AGE));
+            newMeta.setHttpExpiresDate(new Date(getExpirationTimeByMaxAge(MAX_AGE)));
 
-            CopyObjectRequest request = new CopyObjectRequest(bucketName, fileKey, bucketName, fileKey)
+            CopyObjectRequest request = new CopyObjectRequest(YOUR_BUCKET_NAME, fileKey, YOUR_BUCKET_NAME, fileKey)
                     .withNewObjectMetadata(newMeta);
 
             s3.copyObject(request);
         }
     }
 
-    static String getContentTypeValue(int format) {
+    private static long getExpirationTimeByMaxAge(long age) {
+        Date now = new Date();
+        return now.getTime() + age * 1000;
+    }
+
+    private static String getContentTypeValue(int format) {
         if (!isSupported(format))
             throw new IllegalArgumentException("unsupported");
 
